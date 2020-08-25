80eb693db3622c9a1e25e6975f9e53b8121d6a5b
Brett Westerlund <drbretto82@gmail.com>
2020-08-25 12:13:20 -0400

    consistency

--- seeds/seed.noteful.sql
+++ seeds/seed.noteful.sql
@@ -1,4 +1,12 @@
-INSERT INTO notes (id, notes_name, notes_content, folder_id)
+
+INSERT INTO folders (folders_id, folders_name)
+VALUES 
+  (1, 'folder 1'),
+  (2, 'folder 2'),
+  (3, 'folder 3');
+
+
+INSERT INTO notes (notes_id, notes_name, notes_content, folders_id)
 VALUES
   (1, 'Note 1', 'Lorem ipsum dolor sit ametque?', 1),
   (2, 'Note 2', 'Lorem ipsum dolor sit ametque?', 1),
@@ -7,11 +15,5 @@ VALUES
   (5, 'Note 5', 'Lorem ipsum dolor sit ametque?', 1), 
   (6, 'Note 6', 'Lorem ipsum dolor sit ametque?', 3), 
   (7, 'Note 7', 'Lorem ipsum dolor sit ametque?', 3);
-
-INSERT INTO folders (folder_id, folder_name)
-VALUES 
-  (1, 'folder 1'),
-  (2, 'folder 2'),
-  (3, 'folder 3');
 
   
