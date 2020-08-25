2d40428a725e6aa45c3756f442a2efd2e02a20e4
Brett Westerlund <drbretto82@gmail.com>
2020-08-25 12:03:23 -0400

    seed

---
+++ seeds/seed.noteful.sql
@@ -0,0 +1,18 @@
+INSERT INTO notes (id, notes_name, notes_content, folder_id)
+VALUES
+  (1, 'Note 1', 'Lorem ipsum dolor sit ametque?', 1),
+  (2, 'Note 2', 'Lorem ipsum dolor sit ametque?', 1),
+  (3, 'Note 3', 'Lorem ipsum dolor sit ametque?', 2), 
+  (4, 'Note 4', 'Lorem ipsum dolor sit ametque?', 2),  
+  (5, 'Note 5', 'Lorem ipsum dolor sit ametque?', 1), 
+  (6, 'Note 6', 'Lorem ipsum dolor sit ametque?', 3), 
+  (7, 'Note 7', 'Lorem ipsum dolor sit ametque?', 3);
+
+INSERT INTO folders (folder_id, folder_name)
+VALUES 
+  (1, 'folder 1'),
+  (2, 'folder 2'),
+  (3, 'folder 3');
+
+
+  psql -U postgres -d noteful -f ./seeds/seed.noteful.sql
